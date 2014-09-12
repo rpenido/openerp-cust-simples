f61535b0e3af73d6064b185e1b83325f937cd9c7
rpenido <romulo.penido@gmail.com>
2014-09-12 20:13:10 +0000

    Adicionando busca por Razão Social

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
 	'version': "0.5",
 	'author': "rpenido",
 	'depends': ['l10n_br_base','l10n_br_account','purchase'],
-	'update_xml': ['view_partner_form.xml', 'view_partner_tree.xml', 'nf_series_report.xml','view_purchase_order_form.xml'],
+	'update_xml': ['view_partner_form.xml', 'view_partner_tree.xml', 'view_partner_filter.xml', 'nf_series_report.xml','view_purchase_order_form.xml'],
 	'demo': [],
 	'installable': True,
 }
