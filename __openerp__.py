d648634bff18640c4c6bfee0acf70f7676082f03
rpenido <romulo.penido@gmail.com>
2014-09-12 16:29:12 +0000

    Mudança do código de cores de cliente

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
 	'name': "Customizacao - Simples Distribuidora",
 	'version': "0.5",
 	'author': "rpenido",
-	'depends': ['l10n_br_base','purchase'],
+	'depends': ['l10n_br_base','l10n_br_account','purchase'],
 	'update_xml': ['view_partner_form.xml', 'view_partner_tree.xml', 'nf_series_report.xml','view_purchase_order_form.xml'],
 	'demo': [],
 	'installable': True,
